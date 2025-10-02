d0cb76fa8aa42c5f9791180a617cf5d343f6440a
simple2k3 <pduytuan2003@gmail.com>
2025-10-02 20:40:32 +0700

    update project QRcode

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,18 +1,18 @@
 import OrderPage from '@/pages/User/OrderPage.vue';
-// import QRcode from '@/pages/User/QRcode.vue';
+import QRcode from '@/pages/User/QRcode.vue';
 
 import { createRouter, createWebHistory } from 'vue-router';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    // {
-    //   path: '/',
-    //   name: 'QR',
-    //   component: QRcode,
-    // },
     {
       path: '/',
+      name: 'QR',
+      component: QRcode,
+    },
+    {
+      path: '/order',
       name: 'OrderPage',
       component: OrderPage,
     },
